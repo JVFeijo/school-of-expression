6a6f2624233f355ac6dadc5a7f4a487eb78420c8
JVFeijo <joaovictor_feijo@hotmail.com>
2020-04-06 21:43:11 -0300

    bread first search foldTree

--- Trees.hs
+++ Trees.hs
@@ -2,13 +2,27 @@ data Tree a = Leaf a | Branch a (Tree a) (Tree a)
               deriving Show
 
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree f (Leaf value) = Leaf (f value)
-mapTree f (Branch value t1 t2) = Branch (f value) (mapTree f t1) (mapTree f t2)
+mapTree f (Leaf v) = Leaf (f v)
+mapTree f (Branch v t1 t2) = Branch (f v) (mapTree f t1) (mapTree f t2)
 
 foldrTree :: (b -> a -> b) -> b -> Tree a -> b
-foldrTree f acc (Leaf a) = f acc a
-foldrTree f acc (Branch value t1 t2) = f (foldTree f (foldTree f acc t2) t1) value
+foldrTree f acc (Leaf v) = f acc v
+foldrTree f acc (Branch v t1 t2) = f (foldrTree f (foldrTree f acc t2) t1) v
 
-t = Branch 3 (Leaf 1) (Leaf 2)
+foldlTree :: (b -> a -> b) -> b -> Tree a -> b
+foldlTree f acc (Leaf a) = f acc a
+foldlTree f acc (Branch v t1 t2) = f (foldlTree f (foldlTree f acc t1) t2) v
 
-main = (print (foldTree (\acc value -> value:acc) [] t))
+foldBfsTree :: (b -> a -> b) -> b -> Tree a -> b
+foldBfsTree f acc t = foldl f acc bfsArray
+  where bfsArray = zipTree t
+        zipTree (Leaf v) = [v]
+        zipTree (Branch v t1 t2) = v:(concat (zipWith (\x y -> x:y:[]) (zipTree t1) (zipTree t2)))
+
+zipTree :: Tree a -> [a]
+zipTree (Leaf v) = [v]
+zipTree (Branch v t1 t2) = v:(concat (zipWith (\x y -> x:y:[]) (zipTree t1) (zipTree t2)))
+
+t = Branch 7 (Branch 5 (Leaf 1) (Leaf 2)) (Branch 6 (Leaf 3) (Leaf 4))
+
+main = (print (zipTree t))
