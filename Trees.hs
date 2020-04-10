47b7f4ea3acaff33a405ca8027442d25e636e194
JVFeijo <joaovictor_feijo@hotmail.com>
2020-04-09 21:37:06 -0300

    foldBfsTree v2

--- Trees.hs
+++ Trees.hs
@@ -16,13 +16,23 @@ foldlTree f acc (Branch v t1 t2) = f (foldlTree f (foldlTree f acc t1) t2) v
 foldBfsTree :: (b -> a -> b) -> b -> Tree a -> b
 foldBfsTree f acc t = foldl f acc bfsArray
   where bfsArray = zipTree t
-        zipTree (Leaf v) = [v]
-        zipTree (Branch v t1 t2) = v:(concat (zipWith (\x y -> x:y:[]) (zipTree t1) (zipTree t2)))
 
 zipTree :: Tree a -> [a]
 zipTree (Leaf v) = [v]
 zipTree (Branch v t1 t2) = v:(concat (zipWith (\x y -> x:y:[]) (zipTree t1) (zipTree t2)))
+ 
+foldBfsTree2 :: (b -> a -> b) -> b -> Tree a -> b
+foldBfsTree2 f acc tree = aux f acc [tree]
+
+aux :: (b -> a -> b) -> b -> [Tree a] -> b
+aux f acc ((Leaf x):[]) = f acc x
+aux f acc ((Leaf x):ts) = f (aux f acc ts) x
+aux f acc ((Branch x t1 t2):ts) = f (aux f acc (ts ++ [t1, t2])) x
+
+root :: Tree a -> a
+root (Leaf x) = x
+root (Branch x t1 t2) = x
 
 t = Branch 7 (Branch 5 (Leaf 1) (Leaf 2)) (Branch 6 (Leaf 3) (Leaf 4))
 
-main = (print (zipTree t))
+main = (print (foldBfsTree2 (\acc x -> x:acc) [] t))
