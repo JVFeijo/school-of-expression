f410d44b3ac1a2a84666ac0983307f5393faad2e
JVFeijo <joaovictor_feijo@hotmail.com>
2020-09-20 19:16:21 -0300

    indentation fix

--- Perimeter.hs
+++ Perimeter.hs
@@ -12,13 +12,12 @@ perimeter (Polygon vs)        = foldl (+) 0 (sides vs)
 perimeter (Ellipse r1 r2)
     | r1 > r2 = ellipsePerim r1 r2
     | otherwise = ellipsePerim r2 r1
-  where ellipsePerim r1 r2
-    = let e       = sqrt (r1^2 - r2^2) / r1
-          s       = scanl aux (0.25 * e^2) [2..]
-          aux s i = nextEl e s i
-          test x  = x > epsilon
-          sSum    = foldl (+) 0 (takeWhile test s)
-        in 2 * r1 * pi * (1 - sSum)
+  where ellipsePerim r1 r2 = let e = sqrt (r1^2 - r2^2) / r1
+                                 s = scanl aux (0.25 * e^2) [2..]
+                                 aux s i = nextEl e s i
+                                 test x  = x > epsilon
+                                 sSum    = foldl (+) 0 (takeWhile test s)
+                                in 2 * r1 * pi * (1 - sSum)
 
 nextEl :: Float -> Float -> Float -> Float
 nextEl e s i = s * (2 * i - 1) * (2 * i - 3) * (e^2) / (4 * i^2)
